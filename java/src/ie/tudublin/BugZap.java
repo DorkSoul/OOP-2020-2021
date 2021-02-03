03e0a2781c8ab27fd7cad92691c8d2656eed4f05
DorkSoul <luke.hallinan@hotmail.com>
2021-02-03 18:01:04 +0000

    it shoots

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -27,12 +27,25 @@ public class BugZap extends PApplet {
     float playerX;
     float playerY;
     float playerWidth = 20;
+    float fireX;
+    float fireY;
+    boolean f;
     
     void drawPlayer(float x, float y, float w)
     {
         
         fill(0, 255, 0);
         square(x, y, w);
+    }
+
+    void fire(float x, float y)
+    {
+
+        fill(255, 255, 255);
+        square(x, y, 6);
+       
+        
+
     }
 
 	public void draw()
@@ -42,5 +55,43 @@ public class BugZap extends PApplet {
         noStroke();
 
         drawPlayer(playerX, playerY, playerWidth);
-	}
+        
+        if(f == true)
+        {
+            fire(fireX, fireY);
+            fireY = fireY - 10;
+
+            if(fireY < 0)
+            {
+                f = false;
+            }
+        }
+
+        
+    }
+    
+    public void keyPressed()
+	{
+		if (keyCode == LEFT)
+		{
+            if (playerX > 0)
+            {
+                playerX = playerX - 5;
+            }
+		}
+		if (keyCode == RIGHT)
+		{
+            if (playerX < 500)
+            {
+                playerX = playerX + 5;
+            }
+		}
+		if (key == ' ')
+		{
+            fireX = playerX + 8;
+            fireY = playerY + 8;
+            f = true;
+		}
+	}	
 }
+
